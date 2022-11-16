c404053c65d6187396d91feb61e9ec63d876c6ff
StefyAvila <stefany@unah.hn>
2022-11-15 18:58:25 -0600

    H2-Sprint4

--- database/factories/BloqueFactory.php
+++ database/factories/BloqueFactory.php
@@ -18,8 +18,11 @@ class BloqueFactory extends Factory
     {
         return [
             'nombreBloque'=>$this->faker->word(),                                  
-            'cantidadLotes'=>$this->faker->numberBetween(1,100),
-            'colindancia'=>$this->faker->text(),
+            'cantidadLotes'=>$this->faker->numberBetween(1,50),
+            'colindanciaN'=>$this->faker->address(),
+            'colindanciaS'=>$this->faker->address(),
+            'colindanciaE'=>$this->faker->address(),
+            'colindanciaO'=>$this->faker->address(),
             'subirfoto'=>$this->faker->imageUrl($width = 640, $height = 480),
         ];
     }
