c0e48c14eb06912d912b7abe1b3faf29a673a67b
Cbucardo <114634259+CarlosBucardo@users.noreply.github.com>
2023-03-12 12:16:57 -0600

    Correcion de validaciones (tablaplanillas, planilla)

--- app/Http/Controllers/PlanillaController.php
+++ app/Http/Controllers/PlanillaController.php
@@ -27,7 +27,17 @@ class PlanillaController extends Controller
 
     public function store(Request $request){
 
-        //
+        $this->validate($request,[
+            'dias'  => ['required','numeric', 'min:1'],
+            'empleado_id' => ['required', 'unique:planillas']
+        ],[
+            'dias.required'=>'Debe ingresar la cantidad de días, no puede estar vacío.',
+            'dias.min'=>'La cantidad de días debe ser al menos de 1 día',
+            'dias.numeric'=>'Solo se permiten números',
+        
+            'empleado_id.required'=>'Debe seleccionar un empleado, no puede estar vacío.',
+            'empleado_id.unique'=>'Ya se agrego este empleado a la planilla.',
+        ]);
         
         $input = $request->all();
          Planilla::create($input);
--- app/Http/Controllers/TablaplanillaController.php
+++ app/Http/Controllers/TablaplanillaController.php
@@ -29,15 +29,16 @@ class TablaplanillaController extends Controller
     public function store(Request $request){
 
         $this->validate($request,[
-            'dias'  => ['required','numeric', 'min:1'],
-            'empleado_id' => ['required', 'unique:planillas']
-        ],[
-            'dias.required'=>'Debe ingresar la cantidad de días, no puede estar vacío.',
-            'dias.min'=>'La cantidad de días debe ser al menos de 1 día',
-            'dias.numeric'=>'Solo se permiten números',
+            'totalp'  => ['min:1.00'],
+            'canEmpleados'  => ['min:1.00'],
 
-            'empleado_id.required'=>'Debe seleccionar un empleado, no puede estar vacío.',
-            'empleado_id.unique'=>'Ya se agrego este empleado a la planilla.',
+            'fechap'  => ['unique:tablaplanillas'],
+        ],[
+            'totalp.min'=>'Debe de agregar al menos un empleado a la tabla',
+            'canEmpleados.min'=>'Debe de agregar al menos un empleado a la tabla',
+            
+            'fechap.unique'=>'La planilla ya ha sido guardada',
+
         ]);
 
         $input = $request->all();
