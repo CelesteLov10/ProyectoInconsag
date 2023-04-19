7daf194d999784f0f8b0a494e03d51e1f53b69c9
CelesteLov10 <93297097+CelesteLov10@users.noreply.github.com>
2023-04-19 15:39:15 -0600

    Factory cliente

--- database/factories/ClienteFactory.php
+++ database/factories/ClienteFactory.php
@@ -20,7 +20,7 @@ class ClienteFactory extends Factory
             'identidadC'=>$this->faker->unique()->numerify('#############'),
             'nombreCompleto'=>$this->faker->name(),
             'telefono'=>$this->faker->unique()->phoneNumber(),
-            'direccion'=>$this->faker->paragraph(),
+            'direccion'=>$this->faker->address(),
             'fechaNacimiento'=>$this->faker->date($format = 'd-m-Y',$max = 2001),
             
         ];
