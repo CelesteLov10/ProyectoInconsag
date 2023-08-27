ddbe66f4e4d282978789345b34c2af1f4cb48773
Estefany-Lopez1996 <estefanyyissel@gmail.com>
2023-08-27 14:00:38 -0600

    Corrección error 6

--- app/Http/Controllers/ConstructoraController.php
+++ app/Http/Controllers/ConstructoraController.php
@@ -27,7 +27,7 @@ class ConstructoraController extends Controller
     public function store(Request $request)
     {
             $this->validate($request,[
-                'nombreConstructora' => ['required'],
+                'nombreConstructora' => ['required','regex:/^([A-ZÁÉÍÓÚÑa-záéíóúñ]+\s{0,1})+$/u'],
                 'direccion' => ['required','min:10','max:150'],
                 'telefono' => ['required','numeric','regex:/^[(2)(3)(8)(9)][0-9]/','unique:constructoras'],
                 'email' => ['required','email','regex:#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,8}$#', 'unique:constructoras'],
@@ -36,7 +36,7 @@ class ConstructoraController extends Controller
 
             'nombreConstructora.required' => 'El nombre de la constructora es obligatorio, no puede estar vacío.',
             'nombreConstructora.alpha' => 'En el nombre sólo se permite letras.',
-            'nombreConstructora.regex' => 'El nombre debe iniciar con mayúscula y solo permite un espacio entre ellos.',
+            'nombreConstructora.regex' => 'El nombre debe iniciar con mayúscula, no debe incluir números y solo permite un espacio entre ellos.',
 
             'direccion.required' => 'La dirección de la constructora es obligatoria, no puede estar vacío.',
             'direccion.min' => 'La dirección es muy corta. Ingrese entre 10 y 150 caracteres',
@@ -50,14 +50,14 @@ class ConstructoraController extends Controller
 
             'email.required' => 'El correo electrónico de la constructora es obligatorio, no puede estar vacío.',
             'email.email' => 'Debe ingresar un correo electrónico válido.',
-            
+
             'fechaContrato.required' => 'Debe seleccionar la fecha de adquisición, no puede estar vacío.',
             ]);
             $input = $request->all();
-            
+
             Constructora::create($input);
                 return redirect()->route('constructora.index')
-                ->with('mensaje', 'Se guardó el registro de la nueva constructora correctamente');         
+                ->with('mensaje', 'Se guardó el registro de la nueva constructora correctamente');
          /** redireciona una vez enviado  */
     }
 
@@ -75,7 +75,7 @@ class ConstructoraController extends Controller
     }
 
     public function update(Request $request, $id){
-        
+
         $this->validate($request,[
             'nombreConstructora'   => ['required'],
             'direccion'       => ['required','min:10','max:150'],
@@ -102,7 +102,7 @@ class ConstructoraController extends Controller
             'email.unique' => 'El correo electrónico ya existe.',
 
             'fechaContrato.required' => 'Debe seleccionar la fecha de adquisición, no puede estar vacío.',
-            
+
         ]);
 
         $constructoras = Constructora::findOrFail($id);
@@ -114,10 +114,10 @@ class ConstructoraController extends Controller
         $constructoras->fechaContrato = $request->input('fechaContrato');
 
         $update = $constructoras->save();
-        
+
         if ($update){
             return redirect()->route('constructora.index')
             ->with('mensajeW', 'Se actualizó la constructora correctamente');
-        } 
+        }
     }
 }
